1f13f9de6b3fd9b245b4d996275a81f79610a76b
Melissa Griffin <melissa.c.griffin@gmail.com>
2015-05-02 19:22:58 -0400

    redo of website

---
+++ app/scripts/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+(function($) {
+  $(document).ready(function() {
+    // Add your jQuery code here
+
+
+
+    $(function(){
+    	$('#menu').slicknav({
+    		duration: 750,
+    		label: '',
+    	});
+    });
+    
+   
+    $('#clinician_search_field').floatlabel();
+    
+
+   
+}); 
+
+})(jQuery);
